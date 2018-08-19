5652a5e32897739f8e7feaeeb3f7838d398a7306
Gary <Gary@Gary-PC>
2018-08-19 19:36:42 -0300

    Una historia logre cargar.

--- HistoriasConPhotoshop/web/js/MisHistorias.js
+++ HistoriasConPhotoshop/web/js/MisHistorias.js
@@ -4,13 +4,27 @@ class Imagenes
     {
         var texto,i;
         
-        for (i=0; i <= 9; i++)
+        for (i=1; i <= 112; i++)
         {
-            texto+="<div class='col-sm-4'><img src='../img/002.jpg'></div>";
+            texto+="<div class='col-sm-4'><img src='../img/"+i+".jpg' width='350' height='250'></div>";
         }
         
         document.querySelector("#historia").innerHTML += texto;
     }
+    
+    static inicializarGaryVSDiego()
+    {
+        var texto,i;
+        
+        for (i=1; i <= 112; i++)
+        {
+            texto+="<div class='col-sm-4'><img src='../img/img/00"+i+".jpg' width='350' height='250'></div>";
+        }
+        
+        document.querySelector("#historiaBatman").innerHTML += texto;
+    }
 }
 
 Imagenes.inicializar();
+
+Imagenes.inicializarGaryVSDiego();
